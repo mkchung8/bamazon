06d06e70aae365ac5e019ea6fc249d0920030198
Miyoung Chung <mkchung.8@gmail.com>
2019-11-02 17:50:02 -0700

    added customer interface

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bamazon
-Node.js &amp; MySQL Homework Assignment (Bamazon) 
+Bamazon is an Amazon-like storefront created using MySQL and Node.js. This app takes in orders from customers and depeletes the stock from the store's inventory. This app is also programmed to track product sales across all departments of the store and also providing a summary of the highest-grossing departments in the store. 
+
+## Database Setup
+In order to run this application, be sure to have MySQL database downloaded to your computer. Visit the MYSQL INSTALLATION PAGE to install the version needed for your operating system. Once installed, the Bamazon database can be accessed with the SQL code found in Bamazon.sql. Run the code inside your MySQL to populate databases, then you will be ready to proceed with the Bamazon interfaces. 
+
+## Interfaces
+
+### Customer View
+
+### Manager View
+
+### Supervisor View
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,156 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var cartArr = [];
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "mkchung8",
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error(err);
+    }
+    startScreen();
+})
+
+function startScreen() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "doWhat",
+                message: "Welcome to Bamazon Customer Interface. What would you like to do?",
+                choices: ["View Items For Sale", "Search For Item", "Contact Us"]
+            }
+        ])
+        .then(function (answer) {
+
+            switch (answer.doWhat) {
+                case 'View Items For Sale':
+                    viewAllItems();
+                    break;
+                case 'Search For Item':
+                    searchItems();
+                    break;
+                case "Contact Us":
+                    contactUs();
+                    break;
+            }
+
+        });
+
+};
+
+
+function viewAllItems() {
+    let query = "SELECT * FROM products";
+    connection.query(query, function (error, results) {
+        if (error) throw error;
+        for (var i = 0; i < results.length; i++) {
+            console.log(`
+
+            Item: ${results[i].product_name}
+            Category: ${results[i].department_name}
+            Price: $${results[i].price}
+            ID Number: ${results[i].item_id}
+            `);
+        }
+        searchItems();
+    })
+
+};
+
+function searchItems() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "itemId",
+                message: "Enter the ID number of the item you wish to purchase.",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "Enter the number of items to purchase.",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(function (input) {
+            let query = "SELECT item_id, product_name, stock_quantity FROM products WHERE ?";
+            let post = { item_id: input.itemId };
+            connection.query(query, post, function (error, results) {
+                if (error) throw error;
+
+                if (results[0].stock_quantity === 0){
+                    console.log("Sorry, out of stock!");
+                    searchItems();
+                }
+
+                if (input.quantity > results[0].stock_quantity){
+                    console.log("Insufficient Quantity!");
+                    searchItems();
+                } else {
+                    addToCart(input.itemId);
+                    inquirer
+                        .prompt([
+                            {
+                                type: "list", 
+                                name: "action", 
+                                message: "What would you like to do?", 
+                                choices: ["Add Another Item", "View Items For Sale", "Checkout"]
+                            }
+                        ])
+                        .then(function(response) {
+                            switch (response.action) {
+                                case 'Add Another Item': 
+                                searchItems();
+                                break;
+                                case 'View Items For Sale':
+                                viewAllItems();
+                                break;
+                                case 'Checkout': 
+                                checkOut();
+                                break;
+                            }
+                        });
+                };
+
+        
+            });
+        });
+
+
+};
+
+function addToCart(item) {
+    cartArr.push(item);
+    console.log(cartArr);
+};
+
+function checkOut() {
+    console.log("checkOut Function Execute")
+};
+
+function contactUs() {
+    console.log(`
+    Here is our Contact Information
+    Email: bamazon@bamazon.com
+    Phone Number: 1-800-BAMAZON
+    Website: www.bamazon.com 
+    `)
+};
